17b5bed5fb715445550998d23e108c14654d6f0b
Emiel Estiévenart <emiel-perijah@hotmail.com>
2024-11-08 19:31:02 +0100

    Add a library as an example and use it
    Library my_math is added as an example
    Visual Studio startup project is added

--- sources/code/main.cpp
+++ sources/code/main.cpp
@@ -1,7 +1,12 @@
+#include "my_maths/my_maths.hpp"
+
 #include <iostream>
+
 
 
 int main(int argc, char* argv[])
 {
+
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "1 + 1 = " << my_maths::add_two_numbers(1, 1) << std::endl;
 }
--- sources/code/private_header.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-void print_hello();
-
-void print_world ();
