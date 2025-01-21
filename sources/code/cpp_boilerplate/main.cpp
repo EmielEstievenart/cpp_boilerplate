94f137112671f3769351caca06d0b69fc476779b
Emiel Estiévenart <emiel-perijah@hotmail.com>
2025-01-21 15:30:34 +0100

    Add clang-tidy to the project

--- sources/code/cpp_boilerplate/main.cpp
+++ sources/code/cpp_boilerplate/main.cpp
@@ -1,21 +1,30 @@
 #include "my_maths/my_maths.hpp"
 
+#include <exception>
 #include <fstream>
 #include <iostream>
 #include <string>
 
-int main(int argc, char* argv[])
+auto main(int /*argc*/, char* /*argv*/[]) -> int
 {
-    std::ifstream myfile("resources/some_data.txt");
-    if (myfile.is_open())
+    try
     {
-        std::string line;
-        while (std::getline(myfile, line))
+        std::ifstream myfile("resources/some_data.txt");
+        if (myfile.is_open())
         {
-            std::cout << line << std::endl;
+            std::string line;
+            while (std::getline(myfile, line))
+            {
+                std::cout << line << '\n';
+            }
+            myfile.close();
         }
-        myfile.close();
+        std::cout << "1 + 1 = " << my_maths::add_two_numbers(1, 1) << '\n';
+        return 0;
     }
-    std::cout << "1 + 1 = " << my_maths::add_two_numbers(1, 1) << std::endl;
-    return 0;
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
 }
